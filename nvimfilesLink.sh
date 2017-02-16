2922e72555f3207ef285ac004d15d16e156c61aa
N0nki <rgstr_land@icloud.com>
2017-02-17 03:27:45 +0900

    fix ln path

--- nvimfilesLink.sh
+++ nvimfilesLink.sh
@@ -1,4 +1,9 @@
 #!/bin/sh
+
 ln -sf ~/neovimfiles/.dein.toml ~/.dein.toml
 ln -sf ~/neovimfiles/.dein_lazy.toml ~/.dein_lazy.toml
-ln -sf ~/neovimfiles/nvim ~/.config
+ln -sf ~/neovimfiles/nvim/init.vim ~/.config/nvim/init.vim
+ln -sf ~/neovimfiles/nvim/colors/gruvbox.vim ~/.config/nvim/colors/gruvbox.vim
+ln -sf ~/neovimfiles/nvim/colors/neodark.vim ~/.config/nvim/colors/neodark.vim
+ln -sf ~/dotfiles/nvim/colors/onedark.vim ~/.config/nvim/colors/onedark.vim
+ln -sf ~/neovimfiles/nvim/colors/tender.vim ~/.config/nvim/colors/tender.vim
